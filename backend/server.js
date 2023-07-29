88392717a8745cc6879d50d65cbba6488d9578ba
Mohd Ismail <smileweb.developer@gmail.com>
2023-07-29 13:32:37 +0530

    finaldeployerr

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://fanciful-bunny-1c7b35.netlify.app/",
     // credentials: true,
   },
 });
